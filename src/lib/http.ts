e1e9ba84051eb860b52d52b7922111e027402a41
Mischback <dev@mischback.de>
2021-10-30 21:28:10 +0200

    Add getRessourcePathByUrl()

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 
+import { lstat } from "fs/promises";
 import {
   createServer,
   IncomingMessage,
@@ -7,6 +8,7 @@ import {
   Server,
   ServerResponse,
 } from "http";
+import { join } from "path";
 
 import { Srv4DevConfig } from "./configure";
 import { Srv4DevError } from "./errors";
@@ -18,19 +20,63 @@ export class Srv4DevHttpError extends Srv4DevError {
   }
 }
 
+class Srv4DevHttpRessourceNotFoundError extends Srv4DevHttpError {
+  resource: string;
+  constructor(unavailableResource: string) {
+    super(`Ressource not found: "${unavailableResource}"`);
+    this.resource = unavailableResource;
+  }
+}
+
 function getHandlerStaticFiles(webRoot: string): RequestListener {
   return (
     request: IncomingMessage,
     response: ServerResponse
   ): Promise<void> => {
     return new Promise((resolve) => {
-      logger.debug(webRoot);
-      logger.debug(request);
       logger.debug(response);
 
-      return resolve();
+      getRessourcePathByUrl(webRoot, request.url)
+        .then(
+          (ressourcePath) => {
+            logger.debug(ressourcePath);
+          },
+          (err) => {
+            logger.debug(err);
+          }
+        )
+        .catch((err) => {
+          logger.debug(err);
+        })
+        .finally(() => {
+          return resolve();
+        });
     });
   };
+}
+
+function getRessourcePathByUrl(
+  webRoot: string,
+  url: string | undefined
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const uri = url ? url : "/";
+
+    lstat(join(webRoot, uri))
+      .then((statObject) => {
+        if (statObject.isDirectory() === true)
+          return resolve(
+            getRessourcePathByUrl(webRoot, join(uri, "index.html"))
+          );
+        else return resolve(join(webRoot, uri));
+      })
+      .catch((err) => {
+        logger.debug(err);
+        return reject(
+          new Srv4DevHttpRessourceNotFoundError(join(webRoot, uri))
+        );
+      });
+  });
 }
 
 export function launchHttpServer(config: Srv4DevConfig): Promise<Server> {
